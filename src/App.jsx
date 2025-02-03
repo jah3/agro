b9ee6909c9ad90ad68f1adb130baa27dad91c08b
jah3 <isecov9@gmail.com>
2025-02-03 22:20:58 +0200

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <header></header>
             <main>
                 <AppRoutes />
             </main>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
