3fdfd606355dd23ba965d73df03ddc18223c161b
jah3 <isecov9@gmail.com>
2025-02-14 14:57:56 +0200

    main

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/",
+  build: {
+    rollupOptions: {
+      external: ['framer-motion']
+    }
+  }
+
 })
